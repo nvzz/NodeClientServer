647995456610d2560de615883dba4036c20e8bd8
nvzz <n.vazquez05bm@gmail.com>
2019-02-01 17:51:12 -0600

    Exercise node.js

--- client.js
+++ client.js
@@ -1,14 +1,19 @@
+'use strict'
+
 const net = require('net');
+const server = require('./service');
 
 let newClient = net.connect(8080, 'localhost', () =>{
 
     console.log('Enter your name');
-
+ 
     process.stdin.on('data', data => {
-        newClient.write(data)
+        newClient.write(data);
+        
     });
 
     newClient.on('data', data => {
         process.stdout.write(data);
+        process.stdout.write(`${username.server} say: `);
     });
 });
--- service.js
+++ service.js
@@ -4,35 +4,44 @@ const server = require('net').createServer();
 const { StringDecoder } = require('string_decoder');
 const decoder = new StringDecoder('utf8');
 
-
-
 server.on('connection', socket => {
     console.log('client connected');
     let username = false;
-
+    
     socket.on('data', data => {
+
         let msg = decoder.write(data).trim();
 
         if(!username){
+
             username = msg;
+
+            if(username.toLowerCase() === 'admin'){
+                socket.write('Invalid name.\n');
+                socket.end();
+            } 
             if(username === 'Maria'){
                 socket.write('Maria you cannot be here, bye \n');
                 socket.end();
             }
+
             process.stdout.write(`${username} joined\n`);
+
         } else {
             process.stdout.write(`${username} says: ${msg}\n`);
         }
 
         if(msg === 'logout'){
             socket.end();
-        }
+        } 
 
     });
 
     socket.on('end', () => {
-        console.log('Good bye');
-    })
+        //process.stdout.write();
+        console.log(`El usuario ${username} se ha desconectado`);
+    });
+   
 });
 
 server.listen(8080, () => {
